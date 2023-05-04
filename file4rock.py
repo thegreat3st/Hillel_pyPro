af9ec9d5258e2d63a948b147b23a64621a4c58f4
Hillel_pyPro Continuous Integration <username@users.noreply.github.com>
2023-05-04 12:07:41 +0000

    Continuous Integration Build Artifacts

--- file4rock.py
+++ file4rock.py
@@ -1,10 +1,11 @@
 from gg import mapath, file_name
-from pympler import asizeof 
+from pympler import asizeof
 
-def find_in_file (pattern: str):
-    with open(mapath,'r+') as file:
+
+def find_in_file(pattern: str):
+    with open(mapath, "r+") as file:
         file.truncate(0)
-    with open(file_name, 'r', encoding='latin-1') as file, open(mapath, 'a') as file2:
+    with open(file_name, "r", encoding="latin-1") as file, open(mapath, "a") as file2:
         while True:
             line = file.readline()
             if pattern in line:
@@ -12,17 +13,17 @@ def find_in_file (pattern: str):
                 yield line
             elif not line:
                 break
-    with open(mapath, 'r', encoding='latin-1') as file:
+    with open(mapath, "r", encoding="latin-1") as file:
         lines = file.readlines()
         for line in lines:
             print(line)
-    print("Size of the file: %d bytes" %asizeof.asizeof(mapath))
-    with open(mapath, 'r', encoding='latin-1') as res:
+    print("Size of the file: %d bytes" % asizeof.asizeof(mapath))
+    with open(mapath, "r", encoding="latin-1") as res:
         lines = len(res.readlines())
-    print('Total Number of lines:', lines)
+    print("Total Number of lines:", lines)
+
+
 results = find_in_file(
-    pattern = input("enter a line: "),)
+    pattern=input("enter a line: "),
+)
 data = list(results)
-
-
-
