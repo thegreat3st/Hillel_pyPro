52d089b615bcd61dfa041d601fbb63e58ff92aaa
vadimhillel <131892523+vadimhillel@users.noreply.github.com>
2023-05-04 02:52:19 -0700

    Update file4rock.py

--- file4rock.py
+++ file4rock.py
@@ -4,7 +4,8 @@ from gg import mapath, file_name
 def find_in_file (pattern: str):
     with open(mapath,'r+') as file:
         file.truncate(0)
-    with open(file_name, 'r', encoding='latin-1') as file, open(mapath, 'a') as file2:
+    with open(file_name, 'r', encoding='latin-1') as file, 
+    open(mapath, 'a') as file2:
         while True:
             line = file.readline()
             if pattern in line:
